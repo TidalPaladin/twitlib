7cab3bfd0158caa8be6b5d6f1ed1c47f19e8812c
Chase Waggener <tidalpaladin@gmail.com>
2019-06-01 14:59:11 -0500

    Add WorkerThread/subclass property tests

--- test/test_streaming/test_properties.py
+++ test/test_streaming/test_properties.py
@@ -1,6 +1,77 @@
 import pytest
 import os
+
+from twitlib.streaming import WorkerThread, WriterThread, MirrorThread, MediaDownloaderThread
 from twitlib.streaming import Dispatcher
+
+class TestWorkerProperties():
+
+    @pytest.fixture
+    def worker(self):
+        return WorkerThread()
+
+    @pytest.mark.parametrize('val', [True, False])
+    def test_dry_run(self, worker, val):
+        worker.dry_run = val
+        assert(worker.dry_run == val)
+
+    def test_filters(self, worker):
+        worker.filters = []
+        assert(worker.filters == [])
+
+    @pytest.mark.parametrize('val', [None, 1, 10])
+    def test_loops(self, worker, val):
+        worker.loops = val
+        assert(worker.loops == val)
+
+    def test_loops_validate(self, worker):
+        with pytest.raises(ValueError):
+            worker.loops = -1
+        with pytest.raises(ValueError):
+            worker.loops = 0
+
+class TestWriterProperties(TestWorkerProperties):
+
+    @pytest.fixture
+    def worker(self):
+        return WriterThread()
+
+    def test_dirname(self, worker):
+        worker.dirname = 'dir'
+        assert(worker.dirname == 'dir')
+
+    def test_format(self, worker):
+        worker.format = 'format_str'
+        assert(worker.format == 'format_str')
+
+class TestMediaDownloaderProperties(TestWorkerProperties):
+
+    @pytest.fixture
+    def worker(self):
+        return MediaDownloaderThread()
+
+    def test_dirname(self, worker):
+        worker.dirname = 'dir'
+        assert(worker.dirname == 'dir')
+
+    def test_format(self, worker):
+        worker.format = 'format_str'
+        assert(worker.format == 'format_str')
+
+class TestMirrorProperties(TestWorkerProperties):
+
+    @pytest.fixture
+    def worker(self):
+        return MirrorThread()
+
+    def test_temp_dir(self, worker):
+        worker.temp_dir = 'dir'
+        assert(worker.temp_dir == 'dir')
+
+    def test_api(self, api, worker):
+        worker.api = api
+        assert(worker.api == api)
+
 class TestDispatcherProperties():
 
     @pytest.fixture
